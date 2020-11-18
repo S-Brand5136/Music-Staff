ed177e8e9e871548513667c216b43586843fd8d5
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2020-11-17 20:28:53 -0700

    Added register,login,admin functions, userControl

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 
 connectDB();
@@ -11,6 +13,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
