d5b2a7eb03204d41d8fab6af641a28ada594c422
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2020-12-26 18:46:58 -0700

    Work on profile discussion comment tabs

--- frontend/src/components/ProfileTabTable.js
+++ frontend/src/components/ProfileTabTable.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import DiscussPageItem from "../components/DiscussPageItem";
+import DiscussionListItem from "../components/DiscussionListItem";
 
-function TabPanel(props) {
+// MaterialUI imports
+import {
+  AppBar,
+  Box,
+  makeStyles,
+  List,
+  Tabs,
+  Tab,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  MuiAppbar: {
+    backgroundColor: "#363538",
+  },
+}));
+
+const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
 
   return (
@@ -25,7 +42,7 @@ function TabPanel(props) {
       )}
     </div>
   );
-}
+};
 
 TabPanel.propTypes = {
   children: PropTypes.node,
@@ -33,48 +50,43 @@ TabPanel.propTypes = {
   value: PropTypes.any.isRequired,
 };
 
-function a11yProps(index) {
+const a11yProps = (index) => {
   return {
     id: `simple-tab-${index}`,
     "aria-controls": `simple-tabpanel-${index}`,
   };
-}
+};
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-const ProfileTabTable = () => {
+const ProfileTabTable = ({ userInfo, userProfile }) => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.MuiAppbar}>
         <Tabs
           value={value}
           onChange={handleChange}
-          aria-label="simple tabs example"
+          aria-label="Switch between a users posed discussions and comments"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Discussions" {...a11yProps(0)} />
+          <Tab label="Comments" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        Item One
+        <List>
+          {userProfile.discussions.map((item) => (
+            <DiscussionListItem discussion={item} />
+          ))}
+        </List>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
+        {userProfile.comments.map((item) => (
+          <DiscussPageItem data={item} />
+        ))}
       </TabPanel>
     </div>
   );
